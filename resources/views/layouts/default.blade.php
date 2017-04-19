31c34151d5f44f2fc5196dbd3cb0a2b18a81a9b0
eiengwch <eiengwch@qq.com>
2017-04-19 19:30:55 +0800

    修正 sass 编译不正确问题

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
-    <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    
     <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
