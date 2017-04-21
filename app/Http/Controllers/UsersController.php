442c40ecff2504183d77146889c0176be3cbd4a8
eiengwch <eiengwch@qq.com>
2017-04-21 17:43:46 +0800

    添加 关注 取消关注 粉丝

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -17,12 +17,8 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth', [
-            'only' => ['edit', 'update', 'destroy']
-        ]);
-
-        $this->middleware('guest', [
-            'only' => ['create']
-        ]);
+            'only' => ['edit', 'update', 'destroy', 'followings', 'followers']
+        ]);       
     }
 
     public function index()
@@ -128,4 +124,20 @@ class UsersController extends Controller
         session()->flash('success', '恭喜你，激活成功！');
         return redirect()->route('users.show', [$user]);
     }
+
+    public function followings($id)
+    {
+        $user = User::findOrFail($id);
+        $users = $user->followings()->paginate(30);
+        $title = '关注的人';
+        return view('users.show_follow', compact('users', 'title'));
+    }
+
+    public function followers($id)
+    {
+        $user = User::findOrFail($id);
+        $users = $user->followers()->paginate(30);
+        $title = '粉丝';
+        return view('users.show_follow', compact('users', 'title'));
+    }    
 }
